e3d596af69248db40ce129d35eb8e98cf781e709
Swapnil M Mane <swapnilmmane@gmail.com>
2023-10-06 18:09:42 +0530

    docs: Updated theme.ts reference file

--- references/theme.ts
+++ references/theme.ts
@@ -3,6 +3,9 @@
 import { createTheme } from "@webiny/app-theme";
 import { CSSObject } from "@emotion/core";
 
+import { createTheme } from "@webiny/app-theme";
+import { CSSObject } from "@emotion/react";
+
 // Breakpoints (desktop, tablet, mobile).
 export const breakpoints = {
     desktop: "@media (max-width: 4000px)",
@@ -27,7 +30,8 @@ export const colors = {
 
 // Fonts.
 export const fonts = {
-    font1: "'Source Sans Pro', serif", // Primary.
+    font1: "'IBM Plex Sans', sans-serif;", // Primary.
+    font2: "'Lato', sans-serif;" // Secondary.
 };
 
 // Border radius.
@@ -35,9 +39,21 @@ export const borderRadius = 4;
 
 // Typography.
 const headings = {
-    fontFamily: fonts.font1,
-    //color: colors.color3,
-    WebkitFontSmoothing: "antialiased"
+    fontFamily: fonts.font2,
+    color: colors.color3,
+    WebkitFontSmoothing: "antialiased",
+    // Mobile.
+    [breakpoints["mobile-portrait"]]: {
+        fontSize: "0.8em"
+    },
+    [breakpoints["mobile-landscape"]]: {
+        fontSize: "0.85em"
+    },
+     // For editor.
+     "@container page-editor-canvas (max-width: 767px)": {
+        fontSize: "0.8em"
+    },
+    lineHeight: "150%"
 };
 
 const paragraphs = {
@@ -47,54 +63,6 @@ const paragraphs = {
     lineHeight: 1.35,
     WebkitFontSmoothing: "antialiased"
 };
-
-export const typography = {
-    heading1: { ...headings, fontWeight: "bold", fontSize: 48 },
-    heading2: { ...headings, fontSize: 36 },
-    heading3: { ...headings, fontSize: 30 },
-    heading4: { ...headings, fontSize: 24 },
-    heading5: { ...headings, fontSize: 20 },
-    heading6: { ...headings, fontSize: 18, lineHeight: "1.75rem" },
-    paragraph1: { ...paragraphs, fontSize: 16.5 },
-    paragraph2: {
-        ...paragraphs,
-        fontSize: 12.5,
-        letterSpacing: "0.45px",
-        lineHeight: "19px"
-    },
-    quote: {
-        ...paragraphs,
-        fontWeight: "bold",
-        fontSize: 22
-    },
-    list: { ...paragraphs, fontSize: 17 },
-    // Backwards compatibility.
-    // Paragraphs.
-    "webiny-pb-typography-body": { ...paragraphs, fontSize: 16.5 },
-    "webiny-pb-typography-description": {
-        ...paragraphs,
-        fontSize: 12.5,
-        letterSpacing: "0.45px",
-        lineHeight: "19px"
-    },
-
-    // Headings.
-    "webiny-pb-typography-heading": {
-        h1: { ...headings, fontWeight: "bold", fontSize: 48 },
-        h2: { ...headings, fontSize: 36 },
-        h3: { ...headings, fontSize: 30 },
-        h4: { ...headings, fontSize: 24 },
-        h5: { ...headings, fontSize: 20 },
-        h6: { ...headings, fontSize: 18, lineHeight: "1.75rem" }
-    },
-    // Lists.
-    "webiny-pb-typography-list--primary": {
-        ...paragraphs,
-    },
-    "webiny-pb-typography-list--secondary": {
-        ...paragraphs,
-    }
-} as const; // https://github.com/emotion-js/emotion/issues/1373#issuecomment-498059774
 
 // Buttons.
 const buttons = (overrides: CSSObject) => ({
@@ -121,6 +89,106 @@ const buttons = (overrides: CSSObject) => ({
     }
 });
 
+export const typography = {
+    headings: [
+        {
+            id: "heading1",
+            name: "Heading 1",
+            tag: "h1",
+            styles: { ...headings, fontWeight: "bold", fontSize: 48 }
+        },
+        {
+            id: "heading2",
+            name: "Heading 2",
+            tag: "h2",
+            styles: { ...headings, fontSize: 36 }
+        },
+        {
+            id: "heading3",
+            name: "Heading 3",
+            tag: "h3",
+            styles: { ...headings, fontSize: 30 }
+        },
+        {
+            id: "heading4",
+            name: "Heading 4",
+            tag: "h4",
+            styles: { ...headings, fontSize: 24 }
+        },
+        {
+            id: "heading5",
+            name: "Heading 5",
+            tag: "h5",
+            styles: { ...headings, fontSize: 20 }
+        },
+        {
+            id: "heading6",
+            name: "Heading 6",
+            tag: "h6",
+            styles: { ...headings, fontSize: 18, lineHeight: "1.75rem" }
+        }
+    ],
+    paragraphs: [
+        {
+            id: "paragraph1",
+            name: "Paragraph 1",
+            tag: "p",
+            styles: { ...paragraphs, fontSize: 16.5 }
+        },
+        {
+            id: "paragraph2",
+            name: "Paragraph 2",
+            tag: "p",
+            styles: {
+                ...paragraphs,
+                fontSize: 12.5,
+                letterSpacing: "0.45px",
+                lineHeight: "19px"
+            }
+        }
+    ],
+    quotes: [
+        {
+            id: "quote",
+            name: "Quote",
+            tag: "quoteblock",
+            styles: {
+                ...paragraphs,
+                fontWeight: "bold",
+                fontSize: 22
+            }
+        }
+    ],
+    lists: [
+        { 
+            id: "list", 
+            name: "List 1", 
+            tag: "ul", 
+            styles: { 
+                ...paragraphs, 
+                fontSize: 17 
+            } 
+        },
+        {
+            id: "list-orange-checkmark",
+            name: "List with orange checkmark",
+            tag: "ul",
+            styles: {
+                ...paragraphs,
+                fontSize: 17,
+                 margin: 0,
+                listStyle: "none",
+                'li':{
+                    margin: 0,
+                    background: "url(data:image/svg+xml;base64,PHN2ZyB3aWR0aD0iMjAiIGhlaWdodD0iMjEiIHZpZXdCb3g9IjAgMCAyMCAyMSIgZmlsbD0ibm9uZSIgeG1sbnM9Imh0dHA6Ly93d3cudzMub3JnLzIwMDAvc3ZnIj4KPHBhdGggZmlsbC1ydWxlPSJldmVub2RkIiBjbGlwLXJ1bGU9ImV2ZW5vZGQiIGQ9Ik0yMCAxMC4yNDAyQzIwIDE1Ljc2MyAxNS41MjI4IDIwLjI0MDIgOS45OTk5OSAyMC4yNDAyQzQuNDc3MTQgMjAuMjQwMiAtNy42MjkzOWUtMDYgMTUuNzYzIC03LjYyOTM5ZS0wNiAxMC4yNDAyQy03LjYyOTM5ZS0wNiA0LjcxNzM4IDQuNDc3MTQgMC4yNDAyMzQgOS45OTk5OSAwLjI0MDIzNEMxNS41MjI4IDAuMjQwMjM0IDIwIDQuNzE3MzggMjAgMTAuMjQwMlpNNS45MDI4MSAxMC4wNzY0TDguMjE5NDggMTIuMzkzTDE0LjEwMjggNi41MTUyNUwxNC44ODYyIDcuMjk4NTlMOC4yMTk0OCAxMy45NjUyTDUuMTEzOTIgMTAuODU5N0w1LjkwMjgxIDEwLjA3NjRaIiBmaWxsPSIjRkE1QTI4Ii8+Cjwvc3ZnPgo=) no-repeat 0px 0px",
+                    padding: "0 0 15px 35px",
+                    lineHeight: "120%"
+                }
+            }
+        }
+    ]
+} as const; // https://github.com/emotion-js/emotion/issues/1373#issuecomment-498059774 ;
+
 // Theme object.
 const theme = createTheme({
     breakpoints,
@@ -132,13 +200,6 @@ const theme = createTheme({
                 a: { color: colors.color1 },
                 b: { fontWeight: "bold" },
                 i: { fontStyle: "italic" }
-            },
-            quote: {
-                "blockquote > q": {
-                    quotes: "auto",
-                    "&:before": { content: "open-quote" },
-                    "&:after": { content: "close-quote" }
-                }
             },
             button: {
                 default: buttons({ background: colors.color5, color: colors.color3 }),
@@ -156,42 +217,6 @@ const theme = createTheme({
                     color: colors.color1,
                     "&:hover": { transform: "translateY(-1px)" }
                 })
-            },
-
-            list: {
-                "ul, ol": {
-                    li: {
-                        marginBottom: "12px",
-                        marginLeft: "1.875rem",
-                        position: "relative"
-                    }
-                },
-                ul: {
-                    li: {
-                        "&:before,&:after": {
-                            position: "absolute",
-                            content: '""',
-                            borderRadius: "50%"
-                        },
-                        "&:before": {
-                            backgroundColor: "#90c418",
-                            height: "1.25rem",
-                            width: "1.25rem",
-                            left: "-1.875rem",
-                            top: "0.125rem"
-                        },
-                        "&:after": {
-                            backgroundColor: "#ffffff",
-                            height: "0.5rem",
-                            left: "-1.5rem",
-                            top: "0.5rem",
-                            width: "0.5rem"
-                        }
-                    }
-                },
-                ol: {
-                    listStyleType: "decimal"
-                }
             }
         }
     }
